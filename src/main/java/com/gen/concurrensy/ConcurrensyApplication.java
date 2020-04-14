ffd5c50b733cd6585f13689f7a1f148a5d86165a
gen <1757244573@qq.com>
2020-04-14 21:59:56 +0800

    init

---
+++ src/main/java/com/gen/concurrensy/ConcurrensyApplication.java
@@ -0,0 +1,13 @@
+package com.gen.concurrensy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrensyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrensyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gen/concurrensy/ConcurrensyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gen.concurrensy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrensyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
